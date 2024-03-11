4d4d14868ae928b1411710b8b1ecf96f45a87f5d
PikalaxALT <pikalaxalt@users.noreply.github.com>
2024-03-10 21:33:22 -0400

    unk_02054514.s -> land_data.c

--- include/land_data.h
+++ include/land_data.h
@@ -4,10 +4,12 @@
 #include "script.h"
 
 struct LandData {
-    u8 filler_0000[0x80E2];
+    u8 unk_0000[225];
+    u16 unk_00E2[0x4000];
 };
 
 void sub_02054514(FieldSystem *fieldSystem, u8 a1);
 void sub_0205453C(FieldSystem *fieldSystem);
+u16 *sub_02054550(int a0, LandData *a1);
 
 #endif //POKEHEARTGOLD_LAND_DATA_H
