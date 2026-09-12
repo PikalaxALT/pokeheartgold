3e4161d2700199bd10ada47e40c62bcb0fa18831
PikalaxALT <PikalaxALT@users.noreply.github.com>
2026-09-12 15:09:10 -0400

    ov15_021FE204

--- src/application/bag/bag_app_window.c
+++ src/application/bag/bag_app_window.c
@@ -1,14 +1,9 @@
 #include "application/bag_app_internal.h"
 
-typedef struct UnkStruct_02200908 {
-    int x;
-    int y;
-    int baseTile;
-} UnkStruct_02200908;
-
 void ov15_021FE17C(BagAppData *appData);
 void ov15_021FE1D0(BagAppData *appData);
 void ov15_021FE3E0(BagAppData *appData);
+void ov15_021FE204(BagAppData *appData);
 
 void ov15_021FE020(BagAppData *appData) {
     AddWindowParameterized(appData->bgConfig, &appData->windows[0], GF_BG_LYR_MAIN_1, 0, 18, 32, 6, 4, 0x001);
@@ -34,12 +29,12 @@ void ov15_021FE154(BagAppData *appData) {
     ov15_021FE1D0(appData);
 }
 
-extern const UnkStruct_02200908 ov15_02200908[12];
+extern const int ov15_02200908[12][3];
 
 void ov15_021FE17C(BagAppData *appData) {
     if (appData->windows3[0].bgConfig == NULL) {
         for (int i = 0; i < 12; ++i) {
-            AddWindowParameterized(appData->bgConfig, &appData->windows3[i], GF_BG_LYR_SUB_0, ov15_02200908[i].x, ov15_02200908[i].y, 11, 4, 11, ov15_02200908[i].baseTile);
+            AddWindowParameterized(appData->bgConfig, &appData->windows3[i], GF_BG_LYR_SUB_0, ov15_02200908[i][0], ov15_02200908[i][1], 11, 4, 11, ov15_02200908[i][2]);
         }
     }
 }
@@ -51,5 +46,31 @@ void ov15_021FE1D0(BagAppData *appData) {
             RemoveWindow(&appData->windows3[i]);
             appData->windows3[i].bgConfig = NULL;
         }
+    }
+}
+
+extern const int ov15_022008E8[4][2];
+extern const int ov15_022008D0[3][2];
+
+void ov15_021FE204(BagAppData *appData) {
+    if (appData->windows3[12].bgConfig == NULL) {
+        AddWindowParameterized(appData->bgConfig, &appData->windows3[12], GF_BG_LYR_SUB_0, 12, 7, 11, 4, 11, 0x2CF);
+        FillWindowPixelBuffer(&appData->windows3[12], 0);
+        for (int i = 0; i < 4; ++i) {
+            AddWindowParameterized(appData->bgConfig, &appData->windows3[13 + i], GF_BG_LYR_SUB_0, ov15_022008E8[i][0], ov15_022008E8[i][1], 10, 2, 11, 0x31B + 20 * i);
+            FillWindowPixelBuffer(&appData->windows3[13 + i], 0);
+        }
+        for (int i = 0; i < 3; ++i) {
+            AddWindowParameterized(appData->bgConfig, &appData->windows3[17 + i], GF_BG_LYR_SUB_0, ov15_022008D0[i][0], ov15_022008D0[i][1], 2, 3, 11, 0x2FB + 6 * i);
+            FillWindowPixelBuffer(&appData->windows3[17 + i], 0);
+        }
+        AddWindowParameterized(appData->bgConfig, &appData->windows3[20], GF_BG_LYR_SUB_0, 14, 21, 7, 2, 11, 0x30D);
+        FillWindowPixelBuffer(&appData->windows3[20], 0);
+        AddWindowParameterized(appData->bgConfig, &appData->windows3[21], GF_BG_LYR_SUB_0, 11, 1, 18, 4, 11, 0x31B);
+        FillWindowPixelBuffer(&appData->windows3[21], 0);
+        AddWindowParameterized(appData->bgConfig, &appData->windows3[22], GF_BG_LYR_SUB_0, 0, 0, 9, 4, 11, 0x363);
+        FillWindowPixelBuffer(&appData->windows3[22], 0);
+        AddWindowParameterized(appData->bgConfig, &appData->windows3[23], GF_BG_LYR_SUB_0, 24, 14, 8, 3, 11, 0x387);
+        FillWindowPixelBuffer(&appData->windows3[23], 0);
     }
 }
